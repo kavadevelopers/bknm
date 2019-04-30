ba4baf9ccdcd07aecec256c968ba5861710cb39b
Mehul Kava <mehul9921@gmail.com>
2019-04-30 17:46:03 +0530

    Update sidebar.php

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -35,8 +35,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
     
         <a href="<?php echo base_url('dashboard'); ?>" class="brand-link">
-            <img src="<?php echo base_url(); ?><?=$this->config->config["logoFile"]?>" alt="<?=$this->config->config["projectName"]?> logo" class="brand-image img-circle elevation-3"
-               style="opacity: .8">
+            
             <span class="brand-text font-weight-light">
                 <?=$this->config->config["projectName"]; ?>        
             </span>
@@ -48,7 +47,7 @@
       
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
-                    <img src="<?php echo base_url(); ?>uploads/<?=$user->image;?>" style="width: 40px; height:40px;" class="img-circle elevation-2" alt="User Image">
+                    <img src="<?php echo base_url(); ?><?=$this->config->config["logoFile"]?>" style="width: 40px; height:40px;" class="img-circle elevation-2" alt="User Image">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block">
